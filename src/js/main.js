7443d40333a50f64fdd8c884c61d1c14d4d98887
Anton <antemionoy@gmail.com>
2019-07-29 18:58:37 +0300

    finale main page

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 //= ../../bower_components/owl.carousel/dist/owl.carousel.js
 //= ../../bower_components/magnific-popup/dist/jquery.magnific-popup.js
 //= ../../bower_components/remodal/dist/remodal.min.js
+//= ../../bower_components/jquery-mask-plugin/dist/jquery.mask.js
 
 "use strict";
 
@@ -44,6 +45,7 @@ function formAjax() {
 
 
 function changeCheckbox(checkbox, btn) {
+
     var elementCheckbox = $(checkbox);
     var elementBtn = $(btn);
 
@@ -54,6 +56,7 @@ function changeCheckbox(checkbox, btn) {
             $(this).parents('form').find(elementBtn).addClass('btn_disabled');
         }
     });
+    
 }
 
 
@@ -85,10 +88,47 @@ function showLabel() {
 
 }
 
+function scrollToElement(classItem) {
+        $(classItem).on("click", function(t) { 
+            t.preventDefault(); 
+            var e = $($(this).attr("href"));
+
+            $("body,html").animate({ scrollTop: e.offset().top }, 800);
+    });
+}
+
+
+
+function changeCheckbox(checkbox, btn) {
+    var elementCheckbox = $(checkbox);
+    var elementBtn = $(btn);
+
+    elementCheckbox.click(function() {
+        if ($(this).is(':checked')) {
+            $(this).parents('form').find(elementBtn).removeClass('btn_disabled');
+        } else {
+            $(this).parents('form').find(elementBtn).addClass('btn_disabled');
+        }
+    });
+}
+
+
+function inputmask() {
+
+    $("input[type='tel']").mask("+7 (999) 999-99-99");
+
+}
+
 
 
 $(function() {
 
+    inputmask();
+
+    changeCheckbox('.terms-conditions input', '.btn');
+
     showLabel();
+
+    scrollToElement('.anchors__link');
 
 });
